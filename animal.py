20fab607c5cf734835491166afa433efe6390610
Siddiksid <siddiksid15@gmail.com>
2019-02-02 17:11:58 +0530

    oops with animals

--- animal.py
+++ animal.py
@@ -7,17 +7,38 @@ class Animal(object):
 		self.color=color
 	def get_age(self):
 		return self.age
+
 	def get_name(self):
 		return self.name
+
 	def set_name(self,newname=" "):
 		self.name=newname
+
 	def set_age(self,age):
 		self.age=age
+
+	def get_height(self):
+		return self.height
+
+	def get_weight(self):
+		return self.weight
+
+	def get_color(self):
+		return self.color
+
+	def set_height(self,newheight):
+		self.height=newheight
+
+	def set_weight(self,newweight=0):
+		self.weight=newweight
+
+	def set_color(self,newcolor):
+		self.color=newcolor
+
 	def __str__(self):
 		return "animals:"+str(self.age)+":"+str(self.name)
-	def who_is_this(self):
-		print("this is panda")
+	
 
-john=Animal('john',15,6,300,'green')
+
 
 
